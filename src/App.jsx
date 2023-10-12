44ef1a6ecbfcfb3a9ab03b99c6fd1e02dc4886f6
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-12 20:39:31 +0300

    use Global Style, Navigation and Header components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import GlobalStyle from "./util/globalStyles.js";
+import Navigation from "./components/layout/Navigation.jsx";
+import Header from "./components/layout/Header.jsx";
+
 export default function App() {
     return (
         <div>
+            <GlobalStyle/>
+            <Navigation/>
+            <Header/>
         </div>
     )
 }
