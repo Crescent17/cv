005395592f6c36a27dcdfcab33bf2a4ba056f140
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-12 20:39:51 +0300

    create Tech Card component

---
+++ src/components/technologies/TechCard.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+const TechCardStyled = styled.div`
+  position: relative;
+  max-width: 100%;
+  padding: 3rem 2rem;
+  background: linear-gradient(to right, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("../../../public/img/starry-night-sky.jpg");
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: -2px 5px 2px black;
+  transition: all .2s;
+  overflow: hidden;
+
+  &:hover {
+    box-shadow: -2px 5px 5px black;
+    transform: translateY(-2rem);
+  }
+`
+
+export function TechCard({children}) {
+    return (
+        <TechCardStyled>
+            {children}
+        </TechCardStyled>
+    )
+}
