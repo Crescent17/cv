4aa159e1b8a2ed9580067db3f017eb3e36a32381
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-16 13:19:49 +0300

    configure Intl Provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import Navigation from "./components/layout/Navigation.jsx";
 import Header from "./components/layout/Header.jsx";
 import Technologies from "./components/technologies/Technologies.jsx";
 import Slider from "./components/slider/Slider.jsx";
+import ChangeLanguage from "./components/language/ChangeLanguage.jsx";
+import {IntlProvider} from "react-intl";
+import {useState} from "react";
+import translations from './util/translations.json'
 
 export default function App() {
+    const [locale, setLocale] = useState('en')
+    const messages = translations[locale]
     return (
         <div>
             <GlobalStyle/>
-            <Navigation/>
-            <Header/>
-            <Technologies/>
-            <Slider/>
+            <IntlProvider locale={locale} messages={messages}>
+                <ChangeLanguage setLocale={setLocale}/>
+                <Navigation/>
+                <Header/>
+                <Technologies/>
+                <Slider/>
+            </IntlProvider>
         </div>
     )
 }
