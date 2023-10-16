dcd0807aa71964dfc6fdc8b5dc339f78a23931bc
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-16 17:51:51 +0300

    create slides by fetching data from json and mapping through it, heading, description and button for slides

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -5,6 +5,8 @@ import {styleVariables} from "../../util/styleVariables.js";
 import SlideImage from "./SlideImage.jsx";
 import GridContainer from "./GridContainer.jsx";
 import Buttons from "./Buttons.jsx";
+import translations from "../../util/translations.json"
+import {FormattedMessage, useIntl} from "react-intl";
 
 const SliderStyled = styled.section`
   position: relative;
@@ -12,7 +14,7 @@ const SliderStyled = styled.section`
   max-width: 100rem;
   border-radius: 20px 20px 0 0;
   overflow: hidden;
-  margin: 0 auto;
+  margin: 0 auto 30rem;
 `
 
 const TextBlock = styled.div`
@@ -23,28 +25,75 @@ const TextBlock = styled.div`
   height: 100%;
   grid-column: 2 / 3;
   background: ${styleVariables.colors.gradient};
+  display: flex;
+  flex-direction: column;
 `
 
-const numberOfSlides = 3
+const Heading = styled.h2`
+  text-align: center;
+  grid-column: 1 / -1;
+  justify-self: center;
+  color: #fff;
+  font-size: 4rem;
+  text-shadow: -4px 4px 1px black;
+  transition: all .2s;
+  letter-spacing: 10px;
+  z-index: 1;
+  text-transform: uppercase;
+  margin-bottom: 2rem;
+
+  &:hover {
+    transform: skewX(10deg) scale(1.1);
+  }
+`
+
+const Description = styled.p`
+  font-size: 2.4rem;
+`
+
+const Button = styled.a`
+  display: inline-block;
+  background-color: #000;
+  color: inherit;
+  cursor: pointer;
+  padding: 1rem 3rem;
+  border-radius: 10px;
+  text-transform: uppercase;
+  letter-spacing: .9rem;
+  align-self: center;
+  justify-self: end;
+  margin-top: auto;
+  margin-bottom: 10rem;
+  font-size: 3rem;
+  transition: all .2s;
+  border: 1px solid transparent;
+
+  &:hover {
+    color: #da1146;
+    border: 1px solid #fff;
+    background-color: transparent;
+    transform: translateY(-.5rem);
+  }
+`
+
+const numberOfSlides = translations.en["slider-projects"].length
 
 export default function Slider() {
     const [currentSlide, setCurrentSlide] = useState(1)
+    const locale = useIntl().locale
 
     return (
         <SliderStyled id="projects" $number={currentSlide}>
             <Buttons currentSlide={currentSlide} setCurrentSlide={setCurrentSlide} numberOfSlides={numberOfSlides}/>
-            <GridContainer number={1} currentSlide={currentSlide}>
-                <SlideImage img='1.png'/>
-                <TextBlock>Omnifood</TextBlock>
-            </GridContainer>
-            <GridContainer number={2} currentSlide={currentSlide}>
-                <SlideImage img='2.png'/>
-                <TextBlock>Natours</TextBlock>
-            </GridContainer>
-            <GridContainer number={3} currentSlide={currentSlide}>
-                <SlideImage img='3.png'/>
-                <TextBlock>Forkify</TextBlock>
-            </GridContainer>
+            {translations[locale]["slider-projects"].map((project, index) => <GridContainer number={index + 1}
+                                                                                            currentSlide={currentSlide}>
+                <SlideImage img={`${index + 1}.png`}/>
+                <TextBlock>
+                    <Heading>{project.title}</Heading>
+                    <Description>{project.description}</Description>
+                    <Button target="_blank" href={project.link}><FormattedMessage id="button-text"/></Button>
+                </TextBlock>
+            </GridContainer>)}
             <Dots numOfSlides={numberOfSlides} currentSlide={currentSlide} setCurrentSlide={setCurrentSlide}/>
         </SliderStyled>
     )
