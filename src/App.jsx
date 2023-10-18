70b571aaf999e414847bda3fb32e2215a72bf727
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-18 16:46:51 +0300

    create refs for each section, create Main styled component that wraps all the main sections, use new Header and AboutMe components, pass necessary props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import GlobalStyle from "./util/globalStyles.js";
-import Navigation from "./components/layout/Navigation.jsx";
-import Header from "./components/layout/Header.jsx";
+import AboutMe from "./components/layout/AboutMe.jsx";
 import Technologies from "./components/technologies/Technologies.jsx";
 import Slider from "./components/slider/Slider.jsx";
-import ChangeLanguage from "./components/language/ChangeLanguage.jsx";
 import {IntlProvider} from "react-intl";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import translations from './util/translations.json'
 import Footer from "./components/layout/Footer.jsx";
 import {useInView} from "react-intersection-observer";
+import styled from "styled-components";
+import Header from "./components/layout/Header.jsx";
+
+const Main = styled.main`
+  padding: 0 3rem;
+`
 
 export default function App() {
     const [locale, setLocale] = useState('en')
@@ -16,16 +20,21 @@ export default function App() {
     const {ref, inView, entry} = useInView({
         threshold: 0,
     });
+    const techSection = useRef();
+    const projectSection = useRef();
+    const footer = useRef();
     return (
         <>
             <GlobalStyle/>
             <IntlProvider locale={locale} messages={messages}>
-                <ChangeLanguage setLocale={setLocale}/>
-                <Navigation headerInView={entry?.isIntersecting}/>
-                <Header refFn={ref}/>
-                <Technologies/>
-                <Slider/>
-                <Footer/>
+                <Header headerInView={entry?.isIntersecting} setLocale={setLocale} techSection={techSection}
+                            projectSection={projectSection} footer={footer}/>
+                <Main>
+                    <AboutMe refFn={ref}/>
+                    <Technologies techSection={techSection}/>
+                    <Slider projectSection={projectSection}/>
+                </Main>
+                <Footer footer={footer}/>
             </IntlProvider>
         </>
     )
