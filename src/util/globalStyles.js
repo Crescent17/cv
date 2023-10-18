f5f9a12135f346e965c92c2f55e9227045487f51
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-18 16:32:24 +0300

    instead of using background on body element use it on #root element, remove smooth scroll, add media query to change html font size

--- src/util/globalStyles.js
+++ src/util/globalStyles.js
@@ -10,22 +10,25 @@ const GlobalStyle = createGlobalStyle`
       outline: 2px ridge #fa5252;
     }
   }
-  
+
   #root {
     position: absolute;
     width: 100%;
+    background: url("/img/background.jpg") no-repeat;
+    background-size: cover;
   }
 
   html {
     font-size: 62.5%;
-    scroll-behavior: smooth;
     overflow-x: hidden;
+
+    @media (max-width: 95rem) {
+      font-size: 56%;
+    }
   }
 
   body {
     box-sizing: border-box;
-    background: url("/img/background.jpg");
-    background-size: cover;
     font-family: 'Oswald', sans-serif;
     overflow-x: hidden;
     color: #ffdeeb;;
