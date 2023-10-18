708098384b01dc6dcc5c64b0617a94cb8fb13ab4
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-18 17:41:36 +0300

    create more variables for css styles

--- src/util/styleVariables.js
+++ src/util/styleVariables.js
@@ -5,12 +5,14 @@ export const styleVariables = {
     },
     colors: {
         white: "#fff",
+        grey: "#495057",
         darkGrey: "#212529",
         black: "#000",
         hover: "#da1146",
         outline: "#fa5252",
         pinkLight: "#ffdeeb",
         greenLight: "#12b886",
-        gradient: "linear-gradient(225deg, #520088 0%, #106b9f 100%)",
+        mainGradient: "linear-gradient(225deg, #520088 0%, #106b9f 100%)",
+        secondaryGradient: "linear-gradient(112.1deg, rgb(32, 38, 57) 11.4%, rgb(63, 76, 119) 70.2%)"
     }
 }
