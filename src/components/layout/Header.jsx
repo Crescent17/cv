5b6a2dc5ad73d099da6fb634a53699631c6b636a
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-18 16:47:13 +0300

    now Header is a component which will include Logo, Navigation and Open Navigation Menu components, add media queries for it to resize fonts and margins. create state to track if navigation menu is opened and create ref for nav element

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,45 +1,74 @@
 import styled from "styled-components";
-import Avatar from "../avatar/Avatar.jsx";
-import SocialMedia from "../socialMedia/SocialMedia.jsx";
-import TextContent from "../textContent/TextContent.jsx";
-import AnimatedBackground from "../animatedBackground/AnimatedBackground.jsx";
+import {styleVariables} from "../../util/styleVariables.js";
+import Navigation from "./Navigation.jsx";
+import {useRef, useState} from "react";
+import Logo from "../logo/LogoContainer.jsx";
 
-const HeaderStyled = styled.main`
-  max-width: 120rem;
-  padding: 3rem 4rem;
-  background-image: linear-gradient(225deg, #520088 0%, #106b9f 100%);
-  border-radius: 2rem;
-  display: grid;
-  grid-template-columns: 2fr 1fr;
-  grid-template-rows: 1fr auto;
-  margin: 0 auto 30rem;
-  font-size: 1.8rem;
+const HeaderStyled = styled.header`
+  background-image: ${styleVariables.colors.gradient};
+  margin: 3rem auto 10rem;
+  padding: 5rem;
+  width: ${props => props.$headerInView || props.$headerInView === undefined ? '70%' : '100%'};
+  max-height: 4rem;
+  border-radius: ${props => props.$headerInView || props.$headerInView === undefined ? '2rem' : 0};
+  display: flex;
   color: #ffdeeb;
-  z-index: 2;
-  overflow: hidden;
-  position: relative;
-  animation: show 1.5s normal;
+  font-family: 'Poiret One', sans-serif;
+  z-index: 100;
+  position: ${props => props.$headerInView || 'sticky'};
+  top: ${props => props.$headerInView || 0};
+  font-size: 3rem;
+  font-weight: bold;
+  letter-spacing: .1rem;
+  justify-content: space-between;
 
-  @keyframes show {
-    0% {
-      transform: translateX(100%);
-    }
-    80% {
-      transform: translateX(-10%);
-    }
-    100% {
-      transform: translateX(0);
-    }
+  @media (max-width: 95rem) {
+    font-size: 2.5rem;
+  }
+  @media (max-width: 86rem) {
+    font-size: 2rem;
+  }
+  @media (max-width: 74rem) {
+    padding: 5rem 1.6rem;
+  }
+  @media (max-width: 57rem) {
+    width: 100%;
+    border-radius: 0;
+    margin: 0 auto 5rem;
   }
 `
 
-export default function Header({refFn}) {
+const OpenMenu = styled.svg`
+  display: none;
+  cursor: pointer;
+  fill: #fff;
+  width: 4rem;
+  height: 4rem;
+  align-self: center;
+  z-index: ${props => props.$menuIsOpened ? 0 : 500};
+  @media (max-width: 46rem) {
+    display: inline-block;
+    margin-right: 1rem;
+    position: ${props => props.$menuIsOpened ? 'sticky' : 'static'};
+  }
+`
+
+export default function Header({headerInView, setLocale, techSection, projectSection, footer}) {
+    const navElement = useRef()
+    const [menuIsOpened, setMenuIsOpened] = useState(false)
     return (
-        <HeaderStyled ref={refFn}>
-            <AnimatedBackground/>
-            <TextContent/>
-            <Avatar/>
-            <SocialMedia iconSize={70}/>
+        <HeaderStyled $headerInView={headerInView} ref={navElement}>
+            <Logo setLocale={setLocale}/>
+            <Navigation navElement={navElement} techSection={techSection} projectSection={projectSection}
+                        footer={footer} menuIsOpened={menuIsOpened} setMenuIsOpened={setMenuIsOpened}/>
+                <OpenMenu $menuIsOpened={menuIsOpened} onClick={() => setMenuIsOpened(!menuIsOpened)}
+                      xmlns="http://www.w3.org/2000/svg"
+                      x="0px"
+                      y="0px" width="100" height="100"
+                      viewBox="0 0 50 50">
+                    <path
+                        d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+                </OpenMenu>
         </HeaderStyled>
     )
 }
