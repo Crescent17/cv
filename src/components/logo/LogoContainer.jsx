4a62c7b478a6357c04d569efd6f9f2cce9cdd715
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-18 16:42:28 +0300

    create Logo component which will include logo photo, brand name and language switch

---
+++ src/components/logo/LogoContainer.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import ChangeLanguage from "../language/ChangeLanguage.jsx";
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const LogoImg = styled.img`
+  height: 10rem;
+  margin-right: .5rem;
+`
+
+const Brand = styled.p`
+  margin-right: 2rem;
+`
+export default function Logo({setLocale}) {
+    return (
+        <LogoContainer>
+        <LogoImg src="/img/Logo.png" alt="Crescent dev logo"/>
+        <Brand>CRESCENT DEV</Brand>
+        <ChangeLanguage setLocale={setLocale}/>
+    </LogoContainer>
+    )
+}
