a7bf943486362a2304009f61145beacc36949dac
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-18 17:38:11 +0300

    create more variables for css styles

--- src/util/styleVariables.js
+++ src/util/styleVariables.js
@@ -1,5 +1,16 @@
-export const styleVariables =  {
+export const styleVariables = {
+    fonts: {
+        poiretOne: "'Poiret One', sans-serif;",
+        oswald: "'Oswald', sans-serif",
+    },
     colors: {
-        gradient: "linear-gradient(225deg, #520088 0%, #106b9f 100%)"
+        white: "#fff",
+        darkGrey: "#212529",
+        black: "#000",
+        hover: "#da1146",
+        outline: "#fa5252",
+        pinkLight: "#ffdeeb",
+        greenLight: "#12b886",
+        gradient: "linear-gradient(225deg, #520088 0%, #106b9f 100%)",
     }
 }
