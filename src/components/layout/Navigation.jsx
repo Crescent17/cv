4ff310aea62a8ccaa959ed80d0a080745b33f2e1
Marat <94042173+Crescent17@users.noreply.github.com>
2023-10-18 16:51:15 +0300

    change styling, create media queries for mobile navigation, add CloseMenu styled component, create scrollTo function that will scroll to the section with an offset that's equal to the height of the navbar

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -1,20 +1,22 @@
 import styled from "styled-components";
-import {styleVariables} from "../../util/styleVariables.js";
 import {FormattedMessage} from "react-intl";
+import {styleVariables} from "../../util/styleVariables.js";
 
 const Nav = styled.nav`
-  background-image: ${styleVariables.colors.gradient};
-  margin: 3rem auto 10rem;
-  padding: 5rem;
-  width: ${props => props.$headerInView || props.$headerInView === undefined ? '70%' : '100%'};
-  max-height: 4rem;
-  border-radius: ${props => props.$headerInView || props.$headerInView === undefined ? '2rem' : 0};
   display: flex;
-  color: #ffdeeb;
-  font-family: 'Poiret One', sans-serif;
-  z-index: 100;
-  position: ${props => props.$headerInView || 'sticky'};
-  top: ${props => props.$headerInView || 0};
+  transition: all 2s;
+  background: ${styleVariables.colors.gradient};
+  @media (max-width: 46rem) {
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100%;
+    position: fixed;
+    align-items: center;
+    z-index: 50;
+    display: ${props => props.$menuIsOpened ? 'flex' : 'none'};
+    font-size: 4rem;
+  }
 `
 
 const Ul = styled.ul`
@@ -24,49 +26,80 @@ const Ul = styled.ul`
   gap: 7rem;
   align-items: center;
   width: 100%;
-  font-weight: bold;
-  font-size: 3rem;
+  @media (max-width: 74rem) {
+    gap: 4rem;
+  }
+  @media (max-width: 46rem) {
+    flex-direction: column;
+  }
 `
 
 const Li = styled.li`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  letter-spacing: .1rem;
-  font-weight: 600;
+  font-weight: 400;
 
-  &:first-child {
-    font-weight: 400;
-    margin-right: auto;
-  }
-
-  & a {
+  & p {
+    cursor: pointer;
     transition: all 1s;
-  }
 
-  & a:hover {
-    color: #da1146;
+    &:hover {
+      color: #da1146;
+    }
   }
 `
 
-const Img = styled.img`
-  height: 10rem;
-  margin-right: 2rem;
+const CloseMenu = styled.svg`
+  display: none;
+  cursor: pointer;
+  fill: #fff;
+  width: 4rem;
+  height: 4rem;
+  align-self: center;
+  z-index: 20;
+  @media (max-width: 46rem) {
+    display: block;
+    margin-right: 1rem;
+    position: absolute;
+    top: 3rem;
+    right: 1.5rem;
+  }
 `
 
-export default function Navigation({headerInView}) {
-    console.log(headerInView)
+
+function scrollTo(offSetOfElement, scrollToElement) {
+    window.scrollTo({
+        top: scrollToElement.current.offsetTop - offSetOfElement.current.offsetHeight,
+        behavior: "smooth"
+    })
+}
+
+export default function Navigation({navElement, techSection, projectSection, footer, menuIsOpened, setMenuIsOpened}) {
     return (
-        <Nav $headerInView={headerInView}>
+        <Nav $menuIsOpened={menuIsOpened}>
             <Ul>
-                <Li>
-                    <Img src="/img/Logo.png" alt="Crescent dev logo"/>
-                    CRESCENT DEV
+                <Li><p onClick={() => {
+                    setMenuIsOpened(false)
+                    scrollTo(navElement, techSection)
+                }}><FormattedMessage id="technologies"/>
+                </p>
                 </Li>
-                <Li><a href="#tech"><FormattedMessage id="technologies"/></a></Li>
-                <Li><a href="#projects"><FormattedMessage id="projects"/></a></Li>
-                <Li><a href="#footer"><FormattedMessage id="contact-me"/></a></Li>
+                <Li><p onClick={() => {
+                    setMenuIsOpened(false)
+                    scrollTo(navElement, projectSection)
+                }}><FormattedMessage id="projects"/>
+                </p>
+                </Li>
+                <Li><p onClick={() => {
+                    setMenuIsOpened(false)
+                    scrollTo(navElement, footer)
+                }}><FormattedMessage id="contact-me"/></p></Li>
             </Ul>
+            {menuIsOpened && <CloseMenu $menuIsOpened={menuIsOpened} onClick={() => setMenuIsOpened(false)}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        x="0px"
+                                        y="0px" width="50" height="50" viewBox="0 0 50 50">
+                <path
+                    d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z"></path>
+            </CloseMenu>}
         </Nav>
     )
 }
